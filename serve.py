cb07e68beffbf9227f0c79d4110274cb4b96546d
Jim Keener <jim@jimkeener.com>
2014-10-25 11:02:56 -0400

    Check if Feature is under click point and unique
    features_at_point seems to use the bounding box and/or a buffer for each
    feature. As a result the numbr of features returned is much smaller than
    the whole set but not exactly what I want.  As such, lets loop through
    the filtered features and check if the click point is really inside it
    
    Also, my block-group shp file seems to have duplicate shapes in it.  I
    need to clean that up; for now, the same layer shouldn't so we can just
    filter.

--- serve.py
+++ serve.py
@@ -27,11 +27,39 @@ def tile_render(layer, z, x, y):
 @app.route('/query/<layer>/<x>/<y>')
 def query(layer, x, y):
     p = mapnik.Coord(float(x),float(y))
+
+    # This needs to be wrapped in str() in order for 
+    # it to be correctly passed to the C lib
+    # NB: w/o str() and with it both have a type of str
+    ogr_pt = str("POINT ("+x+" "+y+")")
+    ogr_pt= ogr.Geometry(ogr.wkbUnknown, ogr_pt)
+
     feats = []
+
+    has_seen = set()
     if layer in renderers:
         for l in renderers[layer].m.layers:
+            print "Querying from " + l.name + " @ " + str(p)
             for feat in l.datasource.features_at_point(p):
-                feats.append(json.loads(feat.to_geojson()))
+                # features_at_point seems to use the bounding box
+                # and/or a buffer for each feature. As a result the numbr
+                # of features returned is much smaller than the whole set
+                # but not exactly what I want.  As such, lets loop through
+                # the filtered features and check if the click point is
+                # really inside it
+                go_forward = False
+                for i in range(0, feat.num_geometries()):
+                    # No idea why to_wkb dosn't work
+                    geom = ogr.Geometry(ogr.wkbUnknown, feat.get_geometry(i).to_wkt())
+                    go_forward = go_forward or geom.Intersects(ogr_pt)
+
+                # If the point's really inside this feature, and we havn't
+                # seen this feature before (my shp has duplicates?)
+                if go_forward:
+                    internal_id = l.name + "|" + feat.attributes['geoid']
+                    if internal_id not in has_seen:
+                        feats.append(json.loads(feat.to_geojson()))
+                        has_seen.add(internal_id)
         return json.dumps(feats), 200, {
             'Content-Type': 'application/json',
             'Cache-Control': 'max-age=0'
